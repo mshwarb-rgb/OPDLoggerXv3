8509b00d4b1853c8e97b167845f248859fb7eb68
mshwarb-rgb <m.shwarb@gmail.com>
2026-03-18 16:46:07 +0200

    Add files via upload

---
+++ src/dx.js
@@ -0,0 +1,128 @@
+export const DX = [
+  {
+    "id": 1,
+    "code": "AWD",
+    "name": "Acute watery diarrhea",
+    "cat": "M"
+  },
+  {
+    "id": 2,
+    "code": "ABD",
+    "name": "Bloody diarrhea",
+    "cat": "M"
+  },
+  {
+    "id": 3,
+    "code": "SCAB",
+    "name": "Scabies",
+    "cat": "M"
+  },
+  {
+    "id": 4,
+    "code": "SKINF",
+    "name": "Skin infection",
+    "cat": "M"
+  },
+  {
+    "id": 5,
+    "code": "OSKIN",
+    "name": "Other skin visits",
+    "cat": "M"
+  },
+  {
+    "id": 6,
+    "code": "HTN",
+    "name": "Hypertension",
+    "cat": "M"
+  },
+  {
+    "id": 7,
+    "code": "DM",
+    "name": "Diabetes",
+    "cat": "M"
+  },
+  {
+    "id": 8,
+    "code": "RTI",
+    "name": "Respiratory infection",
+    "cat": "M"
+  },
+  {
+    "id": 9,
+    "code": "GU",
+    "name": "Genitourinary visits",
+    "cat": "M"
+  },
+  {
+    "id": 10,
+    "code": "MSK",
+    "name": "MSK (medical)",
+    "cat": "M"
+  },
+  {
+    "id": 11,
+    "code": "GI",
+    "name": "Gastrointestinal (other)",
+    "cat": "M"
+  },
+  {
+    "id": 12,
+    "code": "EYE",
+    "name": "Eye problems",
+    "cat": "M"
+  },
+  {
+    "id": 13,
+    "code": "ENT",
+    "name": "ENT",
+    "cat": "M"
+  },
+  {
+    "id": 14,
+    "code": "NEU",
+    "name": "Neurology",
+    "cat": "M"
+  },
+  {
+    "id": 15,
+    "code": "PSY",
+    "name": "Psychiatry",
+    "cat": "M"
+  },
+  {
+    "id": 16,
+    "code": "OTHM",
+    "name": "Other medical",
+    "cat": "M"
+  },
+  {
+    "id": 17,
+    "code": "WOUND",
+    "name": "Wound/Dressing",
+    "cat": "S"
+  },
+  {
+    "id": 18,
+    "code": "BURN",
+    "name": "Burn",
+    "cat": "S"
+  },
+  {
+    "id": 19,
+    "code": "FRACT",
+    "name": "Fracture/Ortho",
+    "cat": "S"
+  },
+  {
+    "id": 20,
+    "code": "GSW",
+    "name": "GSW/Blast",
+    "cat": "S"
+  },
+  {
+    "id": 21,
+    "code": "OTHS",
+    "name": "Other surgical",
+    "cat": "S"
+  }
+];
